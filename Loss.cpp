2a5c71580a1e527e852df2f23e6ca5ab8ef42a8a
christopherahk <christopher.ahk@gmail.com>
2026-02-25 13:58:04 +0100

    Fixed file names for loss files

--- Loss.cpp
+++ Loss.cpp
@@ -1,8 +1,8 @@
 #include "Loss.h"
 #include "Tensor.h"
 
-float Loss::calculate(const Tensor& output, const Tensor& yTrue) {
+float Loss::calculate(const Tensor &output, const Tensor &yTrue) {
   Tensor sampleLosses = forward(output, yTrue);
-  
+
   return sampleLosses.mean();
 }
