37851e4a45502e455eae7d2e395cd7462eddadea
Ro Nanak Prasad Lacoul <nanaklacoulbus@gmail.com>
2026-03-02 15:27:58 +0100

    need to move to branch

--- loss.cpp
+++ loss.cpp
@@ -1,22 +1,8 @@
-#include "loss.h"
+#include "Loss.h"
 #include "Tensor.h"
 
-float computeMSEClassLoss(const Tensor &True_vals, const Tensor &Pred_vals) {
+float Loss::calculate(const Tensor &output, const Tensor &yTrue) {
+  Tensor sampleLosses = forward(output, yTrue);
 
-  // An implementation of MSE multiclass loss function. Not suited for
-  // classification but will be used for testing
-  int N = True_vals.size(); // total entries in the tensor i.e. "N"
-
-  // check if tensors are of same size
-  if (N != Pred_vals.size()) {
-  }
-  float sum = 0.0f;
-  // compute yi - yhat
-  for (int i = 0; i < N; ++i) {
-    float error = True_vals(i, 1) - Pred_vals(i, 1);
-    // square that shit
-    sum += error * error;
-  }
-  // Sum/N, static cast N(int) -> N(float)
-  return sum / static_cast<float>(N);
+  return sampleLosses.mean();
 }
