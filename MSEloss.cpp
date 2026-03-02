fc9e3d8ad2131592340895940ac882e0c7cc62a0
Ro Nanak Prasad Lacoul <nanaklacoulbus@gmail.com>
2026-03-02 11:59:40 +0100

    Update MSEloss.cpp
    was wrong af

--- MSEloss.cpp
+++ MSEloss.cpp
@@ -2,38 +2,24 @@
 #include "Tensor.h"
 #include <cmath>
 
-float computeCCEntropyLossOneHot(const Tensor &True_onehot,
-                                 const Tensor &Pred_vals) {
-  int rows = Pred_vals.rows;
-  int cols = Pred_vals.cols; // num classes
-  float total_loss = 0.0f;
-  const float epsilon = 1e-7f;
+float computeMSEClassLoss(const Tensor &True_vals, const Tensor &Pred_vals) {
 
-  for (int i = 0; i < rows; ++i) {
-    int true_class = -1;
+  int rows = True_vals.rows;
+  int cols = True_vals.cols;
 
-    // One-Hot search
-    for (int j = 0; j < cols; ++j) {
-      if (True_onehot(i, j) > 0.9f) {
-        true_class = j;
-        break; //
-      }
-    }
-
-    // safety check for invalid one-hot encoding
-    if (true_class == -1)
-      continue;
-
-    float y_pred = Pred_vals(i, true_class);
-
-    // Clipping
-    if (y_pred < epsilon)
-      y_pred = epsilon;
-    if (y_pred > 1.0f - epsilon)
-      y_pred = 1.0f - epsilon;
-
-    total_loss += -std::log(y_pred);
+  if (rows != Pred_vals.rows || cols != Pred_vals.cols) {
+    // håndter fejl
   }
 
-  return total_loss / static_cast<float>(rows);
+  float sum = 0.0f;
+
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+
+      float error = True_vals(i, j) - Pred_vals(i, j);
+      sum += error * error;
+    }
+  }
+
+  return sum / static_cast<float>(rows);
 }
