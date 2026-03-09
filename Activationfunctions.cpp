98cfb8787ef2524876fd9732521062795266403c
Liv Sjoerup <s246029@dtu.dk>
2026-03-09 10:51:40 +0100

    fixet aktiveringsfunktioner

--- Activationfunctions.cpp
+++ Activationfunctions.cpp
@@ -2,73 +2,87 @@
 
 // Sigmoid activationfunction and derivative
 
-void sigmoid(float *arr, int size) {
-  for (int i = 0; i < size; i++) {
-    float x = arr[i];
+void sigmoid(Tensor &t) {
+  for (int r = 0; r < t.rows; r++) {
+    for (int c = 0; c < t.cols; c++) {
+      float x = t(r, c);
 
-    if (x >= 0.0f) {
-      arr[i] = 1.0f / (1.0f + std::exp(-x));
-    } else {
-      float z = std::exp(x);
-      arr[i] = z / (1.0f + z);
+      if (x >= 0.0f) {
+        t(r, c) = 1.0f / (1.0f + std::exp(-x));
+      } else {
+        float z = std::exp(x);
+        t(r, c) = z / (1.0f + z);
+      }
     }
   }
 }
 
-void sigmoid_dev(float *arr, int size) {
-  for (int i = 0; i < size; i++) {
-    arr[i] = arr[i] * (1.0f - arr[i]);
+void sigmoid_dev(Tensor &t) {
+  for (int r = 0; r < t.rows; r++) {
+    for (int c = 0; c < t.cols; c++) {
+      t(r, c) = t(r, c) * (1.0f - t(r, c));
+    }
   }
 }
 
 // Relu activationfunction and derivative
-void relu(float *arr, int size) {
-  for (int i = 0; i < size; i++) {
-    if (arr[i] < 0.0f) {
-      arr[i] = 0.0f;
+void relu(Tensor &t) {
+  for (int r = 0; r < t.rows; r++) {
+    for (int c = 0; c < t.cols; c++) {
+      if (t(r, c) < 0.0f) {
+        t(r, c) = 0.0f;
+      }
     }
   }
 }
 
-void relu_dev(float *arr, int size) {
-  for (int i = 0; i < size; i++) {
-    if (arr[i] < 0.0f) {
-      arr[i] = 0.0f;
-    } else {
-      arr[i] = 1.0f;
+void relu_dev(Tensor &t) {
+  for (int r = 0; r < t.rows; r++) {
+    for (int c = 0; c < t.cols; c++) {
+      if (t(r, c) > 0.0f) {
+        t(r, c) = 1.0f;
+      } else {
+        t(r, c) = 0.0f;
+      }
     }
   }
 }
 
 // Leaky relu activationfunction and derivative
-void leaky_relu(float *arr, int size) {
-  for (int i = 0; i < size; i++) {
-    if (arr[i] < 0.0f) {
-      arr[i] = 0.01f * arr[i];
+void leaky_relu(Tensor &t) {
+  for (int r = 0; r < t.rows; r++) {
+    for (int c = 0; c < t.cols; c++) {
+      if (t(r, c) < 0.0f) {
+        t(r, c) = 0.01f * t(r, c);
+      }
     }
   }
 }
 
-void leaky_relu_dev(float *arr, int size) {
-  for (int i = 0; i < size; i++) {
-    if (arr[i] < 0.0f) {
-      arr[i] = 0.01f;
-    } else {
-      arr[i] = 1.0f;
+void leaky_relu_dev(Tensor &t) {
+  for (int r = 0; r < t.rows; r++) {
+    for (int c = 0; c < t.cols; c++) {
+      if (t(r, c) < 0.0f) {
+        t(r, c) = 0.01f;
+      } else {
+        t(r, c) = 1.0f;
+      }
     }
   }
 }
 
 // Softmax activationfunction
-void softmax(float *arr, int size) {
-  float sum = 0.0f;
+void softmax(fTensor &t) {
+  for (int r = 0; r < t.rows; r++) {
+    float sum = 0.0f;
 
-  for (int i = 0; i < size; i++) {
-    arr[i] = std::exp(arr[i]);
-    sum += arr[i];
-  }
+    for (int c = 0; c < t.cols; c++) {
+      t(r, c) = std::exp(t(r, c));
+      sum += t(r, c);
+    }
 
-  for (int i = 0; i < size; i++) {
-    arr[i] = arr[i] / sum;
+    for (int c = 0; c < t.cols; c++) {
+      t(r, c) /= sum;
+    }
   }
 }
