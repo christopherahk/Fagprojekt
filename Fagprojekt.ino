c9eba65246dab2e108ee6dea03feae5dbd45d4da
christopherahk <christopher.ahk@gmail.com>
2026-05-18 13:17:43 +0200

    Changed model to 1D CNN and changed preprocessing. Also trained model for 1 epoch.

--- Fagprojekt.ino
+++ Fagprojekt.ino
@@ -1,56 +1,66 @@
+#define SERIAL_RX_BUFFER_SIZE 256
+
 #include "Activations.h"
 #include "CategoricalCrossEntropyLoss.h"
 #include "Conv2DLayer.h"
 #include "DenseLayer.h"
 #include "GetLoss.h"
-#include "MaxPool2D.h"
+#include "trained_weights.h"
 #include <Arduino.h>
 
+const bool testing = false;
+
 const int N_CHANNELS = 56;
-const int WINDOW = 100;
+const int SEQ_LEN = 16;
 const int N_CLASSES = 3;
-const int N_FLOATS = N_CHANNELS * WINDOW;
+const int N_FLOATS = N_CHANNELS * SEQ_LEN;
 const int BYTES_NEEDED = N_FLOATS * sizeof(float);
 const int CHUNK_SIZE = 256;
 
-const int BATCH_SIZE = 8;
+const int BATCH_SIZE = 32;
 const bool FREEZE_CONV = false;
 const float INITIAL_LR = 0.01f;
 const float LR_DECAY = 0.95f;
 const int DECAY_STEP = 100;
 
-const int CONV_FILTERS = 4;
-const int CONV_KERNEL_H = 7;
-const int CONV_KERNEL_W = 7;
-const int CONV_STRIDE_H = 3;
-const int CONV_STRIDE_W = 4;
-const int CONV_PAD_H = 0;
-const int CONV_PAD_W = 0;
-const int CONV_OUT_H =
-    (N_CHANNELS + 2 * CONV_PAD_H - CONV_KERNEL_H) / CONV_STRIDE_H + 1;
-const int CONV_OUT_W =
-    (WINDOW + 2 * CONV_PAD_W - CONV_KERNEL_W) / CONV_STRIDE_W + 1;
+const int C1_FILTERS = 16;
+const int C1_KH = N_CHANNELS;
+const int C1_KW = 3;
+const int C1_SH = N_CHANNELS;
+const int C1_SW = 1;
+const int C1_PAD_H = 0;
+const int C1_PAD_W = 1;
+const int C1_OUT_H = (N_CHANNELS + 2 * C1_PAD_H - C1_KH) / C1_SH + 1;
+const int C1_OUT_W = (SEQ_LEN + 2 * C1_PAD_W - C1_KW) / C1_SW + 1;
 
-const int POOL_H = 3;
-const int POOL_W = 3;
-const int POOL_OUT_H = CONV_OUT_H / POOL_H;
-const int POOL_OUT_W = CONV_OUT_W / POOL_W;
+const int C2_FILTERS = 32;
+const int C2_KH = C1_FILTERS;
+const int C2_KW = 3;
+const int C2_SH = C1_FILTERS;
+const int C2_SW = 1;
+const int C2_PAD_H = 0;
+const int C2_PAD_W = 1;
+const int C2_OUT_H = (C1_FILTERS + 2 * C2_PAD_H - C2_KH) / C2_SH + 1;
+const int C2_OUT_W = (C1_OUT_W + 2 * C2_PAD_W - C2_KW) / C2_SW + 1;
 
-const int CONV_OUT_SIZE = CONV_FILTERS * POOL_OUT_H * POOL_OUT_W;
-const int HIDDEN_SIZE = 32;
+const int POOL_OUT = C2_FILTERS;
+const int HIDDEN_SIZE = 16;
 const int OUTPUT_SIZE = N_CLASSES;
 
 static float values[N_FLOATS];
-static Tensor flat(1, CONV_OUT_SIZE);
-static Tensor dFlat(CONV_FILTERS, POOL_OUT_H *POOL_OUT_W);
-static Tensor input(N_CHANNELS, WINDOW);
+static Tensor flat(1, POOL_OUT);
+static Tensor dFlat(1, POOL_OUT);
+static Tensor input(N_CHANNELS, SEQ_LEN);
 static Tensor yTrue(1, OUTPUT_SIZE);
 
-Conv2DLayer conv(N_CHANNELS, WINDOW, CONV_FILTERS, CONV_KERNEL_H, CONV_KERNEL_W,
-                 CONV_STRIDE_H, CONV_STRIDE_W, CONV_PAD_H, CONV_PAD_W);
-ReLU reluConv;
-MaxPool2D pool(CONV_FILTERS, CONV_OUT_H, CONV_OUT_W, POOL_H, POOL_W);
-DenseLayer layer1(CONV_OUT_SIZE, HIDDEN_SIZE);
+Conv2DLayer conv1(N_CHANNELS, SEQ_LEN, C1_FILTERS, C1_KH, C1_KW, C1_SH, C1_SW,
+                  C1_PAD_H, C1_PAD_W);
+ReLU reluConv1;
+Conv2DLayer conv2(C1_FILTERS, C1_OUT_W, C2_FILTERS, C2_KH, C2_KW, C2_SH, C2_SW,
+                  C2_PAD_H, C2_PAD_W);
+ReLU reluConv2;
+
+DenseLayer layer1(POOL_OUT, HIDDEN_SIZE);
 ReLU relu1;
 DenseLayer layer2(HIDDEN_SIZE, OUTPUT_SIZE);
 GetLoss getLoss;
@@ -59,6 +69,25 @@ static int windowCount = 0;
 static int batchCounter = 0;
 static int globalBatchCount = 0;
 static float currentLR = INITIAL_LR;
+
+/*void loadWeights() {
+  memcpy(conv1.weights.data, conv1_w, conv1.weights.size * sizeof(float));
+  memcpy(conv1.biases.data, conv1_b, conv1.biases.size * sizeof(float));
+  memcpy(conv1.mWeights.data, conv1_mw, conv1.mWeights.size * sizeof(float));
+  memcpy(conv1.mBiases.data, conv1_mb, conv1.mBiases.size * sizeof(float));
+  memcpy(conv2.weights.data, conv2_w, conv2.weights.size * sizeof(float));
+  memcpy(conv2.biases.data, conv2_b, conv2.biases.size * sizeof(float));
+  memcpy(conv2.mWeights.data, conv2_mw, conv2.mWeights.size * sizeof(float));
+  memcpy(conv2.mBiases.data, conv2_mb, conv2.mBiases.size * sizeof(float));
+  memcpy(layer1.weights.data, layer1_w, layer1.weights.size * sizeof(float));
+  memcpy(layer1.biases.data, layer1_b, layer1.biases.size * sizeof(float));
+  memcpy(layer1.mWeights.data, layer1_mw, layer1.mWeights.size * sizeof(float));
+  memcpy(layer1.mBiases.data, layer1_mb, layer1.mBiases.size * sizeof(float));
+  memcpy(layer2.weights.data, layer2_w, layer2.weights.size * sizeof(float));
+  memcpy(layer2.biases.data, layer2_b, layer2.biases.size * sizeof(float));
+  memcpy(layer2.mWeights.data, layer2_mw, layer2.mWeights.size * sizeof(float));
+  memcpy(layer2.mBiases.data, layer2_mb, layer2.mBiases.size * sizeof(float));
+}*/
 
 void exportModel() {
   Serial.println("START_EXPORT");
@@ -79,10 +108,14 @@ void exportModel() {
     Serial.println("};");
   };
 
-  printTensor("conv_w", conv.weights);
-  printTensor("conv_b", conv.biases);
-  printTensor("conv_mw", conv.mWeights);
-  printTensor("conv_mb", conv.mBiases);
+  printTensor("conv1_w", conv1.weights);
+  printTensor("conv1_b", conv1.biases);
+  printTensor("conv1_mw", conv1.mWeights);
+  printTensor("conv1_mb", conv1.mBiases);
+  printTensor("conv2_w", conv2.weights);
+  printTensor("conv2_b", conv2.biases);
+  printTensor("conv2_mw", conv2.mWeights);
+  printTensor("conv2_mb", conv2.mBiases);
   printTensor("layer1_w", layer1.weights);
   printTensor("layer1_b", layer1.biases);
   printTensor("layer1_mw", layer1.mWeights);
@@ -96,20 +129,43 @@ void exportModel() {
   Serial.println("END_EXPORT");
 }
 
+void printProbs(int labelIdx, float loss) {
+  Tensor probs = getLoss.activation.output;
+  Serial.print("Probs: ");
+  for (int i = 0; i < OUTPUT_SIZE; i++) {
+    Serial.print(probs.data[i], 2);
+    if (i < OUTPUT_SIZE - 1)
+      Serial.print(", ");
+  }
+  Serial.print(" | Label: ");
+  Serial.print(labelIdx);
+  Serial.print(" | Loss: ");
+  Serial.println(loss, 2);
+}
+
 void processWindow(const float *data, const int label[N_CLASSES], int wCount) {
   for (int i = 0; i < N_FLOATS; i++)
     input.data[i] = data[i];
+
   int labelIdx = -1;
   for (int i = 0; i < N_CLASSES; i++) {
     yTrue.data[i] = (label[i] == 1) ? 1.0f : 0.0f;
     if (label[i] == 1)
       labelIdx = i;
   }
-  conv.forward(input);
-  reluConv.forward(conv.output);
-  pool.forward(reluConv.output);
 
-  memcpy(flat.data, pool.output.data, CONV_OUT_SIZE * sizeof(float));
+  conv1.forward(input);
+  reluConv1.forward(conv1.output);
+
+  conv2.forward(reluConv1.output);
+  reluConv2.forward(conv2.output);
+
+  for (int c = 0; c < C2_FILTERS; c++) {
+    float sum = 0.0f;
+    for (int t = 0; t < C2_OUT_W; t++)
+      sum += reluConv2.output.data[c * C2_OUT_W + t];
+    flat.data[c] = sum / C2_OUT_W;
+  }
 
   layer1.forward(flat);
   relu1.forward(layer1.output);
@@ -117,41 +173,43 @@ void processWindow(const float *data, const int label[N_CLASSES], int wCount) {
 
   float loss = getLoss.forward(layer2.output, yTrue);
 
-  getLoss.backward(yTrue);
-  layer2.backward(getLoss.dInputs);
-  relu1.backward(layer2.dInputs);
-  layer1.backward(relu1.dInputs);
+  if (!testing) {
+    getLoss.backward(yTrue);
+    layer2.backward(getLoss.dInputs);
+    relu1.backward(layer2.dInputs);
+    layer1.backward(relu1.dInputs);
 
-  memcpy(dFlat.data, layer1.dInputs.data, CONV_OUT_SIZE * sizeof(float));
-
-  pool.backward(dFlat);
-  reluConv.backward(pool.dInputs);
-  conv.backward(reluConv.dInputs, true);
-
-  batchCounter++;
-  if (batchCounter >= BATCH_SIZE) {
-    float scaledLR = currentLR / BATCH_SIZE;
-    globalBatchCount++;
-    if (globalBatchCount % DECAY_STEP == 0)
-      currentLR *= LR_DECAY;
-    if (!FREEZE_CONV)
-      conv.update(scaledLR);
-    layer1.update(scaledLR);
-    layer2.update(scaledLR);
-    batchCounter = 0;
-  }
-  if (wCount % 10 == 0) {
-    Tensor probs = getLoss.activation.output;
-    Serial.print("Probs: ");
-    for (int i = 0; i < OUTPUT_SIZE; i++) {
-      Serial.print(probs.data[i], 2);
-      if (i < OUTPUT_SIZE - 1)
-        Serial.print(", ");
+    memcpy(dFlat.data, layer1.dInputs.data, POOL_OUT * sizeof(float));
+    for (int c = 0; c < C2_FILTERS; c++) {
+      float g = dFlat.data[c] / C2_OUT_W;
+      for (int t = 0; t < C2_OUT_W; t++)
+        reluConv2.output.data[c * C2_OUT_W + t] = g;
     }
-    Serial.print(" | Label: ");
-    Serial.print(labelIdx);
-    Serial.print(" | Loss: ");
-    Serial.println(loss, 2);
+
+    reluConv2.backward(reluConv2.output);
+    conv2.backward(reluConv2.dInputs, true);
+
+    reluConv1.backward(conv2.dInputs);
+    conv1.backward(reluConv1.dInputs, true);
+
+    batchCounter++;
+    if (batchCounter >= BATCH_SIZE) {
+      float scaledLR = currentLR / BATCH_SIZE;
+      globalBatchCount++;
+      if (globalBatchCount % DECAY_STEP == 0)
+        currentLR *= LR_DECAY;
+      if (!FREEZE_CONV) {
+        conv1.update(scaledLR);
+        conv2.update(scaledLR);
+      }
+      layer1.update(scaledLR);
+      layer2.update(scaledLR);
+      batchCounter = 0;
+    }
+  }
+
+  if ((wCount % 100 == 0 && !testing) || (testing)) {
+    printProbs(labelIdx, loss);
   }
 }
 
@@ -181,6 +239,8 @@ void get_data() {
 
 void setup() {
   Serial.begin(500000);
+  if (testing) {
+  }
   while (true) {
     Serial.println("READY");
     delay(500);
--- python_files/streamer.py
+++ python_files/streamer.py
@@ -4,57 +4,69 @@ from load_intan_rhd_format import read_data
 import os
 import serial
 import random
-import glob
+from sklearn.preprocessing import StandardScaler
 
 PORT = "COM3"
-BAUD = 500_000
+BAUD = 1_000_000
+
 DOWNSAMPLE_FACTOR = 50
 N_CHANNELS = 56
-WINDOW = 100
+SEQ_LEN = 16
 CHUNK_SIZE = 256
-
-EPOCHS = 100
+EPOCHS = 10
+SUBSAMPLE_RATE = 20
 
 CLASSES = {
     "DORSIFLEXION": 0,
     "PLANTARFLEXION": 1,
     "PRICKING": 2
 }
+
 N_CLASSES = len(CLASSES)
 
-
 def load_and_downsample(filename, num_channels=N_CHANNELS, downsample_factor=DOWNSAMPLE_FACTOR):
-    print(f'Loading {filename}')
+    print(f"Loading {filename}")
     result = read_data(filename)
-
-    amplifier_data = result['amplifier_data']
+    data = result['amplifier_data'][:num_channels]
     t = result['t_amplifier']
 
-    total_channels = amplifier_data.shape[0]
-    if num_channels > total_channels:
-        raise ValueError(f'Requested {num_channels} channels but file only has {total_channels}')
+    print("Downsampling")
+    downsampled = np.stack([
+        decimate(data[i], downsample_factor, ftype="fir", zero_phase=True)
+        for i in range(num_channels)
+    ])
 
-    channels = amplifier_data[:num_channels, :]
+    t_ds = t[::downsample_factor][:downsampled.shape[1]]
+    WINDOW_SIZE = 1
+    n_windows = downsampled.shape[1] // WINDOW_SIZE
 
-    print(f'Downsampling by factor {downsample_factor}')
-    num_samples_downsampled = len(decimate(channels[0], downsample_factor))
-    downsampled = np.zeros((num_channels, num_samples_downsampled))
+    downsampled = downsampled[:, :n_windows * WINDOW_SIZE]
+    t_ds = t_ds[:n_windows * WINDOW_SIZE]
 
-    for i in range(num_channels):
-        downsampled[i] = decimate(channels[i], downsample_factor)
+    windows = downsampled.reshape(num_channels, n_windows, WINDOW_SIZE)
+    windows = np.transpose(windows, (1, 0, 2))
 
-    t_downsampled = t[::downsample_factor][:num_samples_downsampled]
+    return t_ds, windows
 
-    actual_windows = downsampled.shape[1] // WINDOW
+def readline(ser):
+    try:
+        return ser.readline().decode("utf-8", errors="replace").strip()
+    except Exception:
+        return ""
 
-    data = downsampled[:, :actual_windows * WINDOW].reshape(num_channels, actual_windows, WINDOW)
-    return t_downsampled, data
-
+def wait_for_answer(ser, window_idx):
+    while True:
+        line = readline(ser)
+        if line.startswith("Probs") and window_idx % 100 == 0:
+            print(f"Arduino: {line}")
+        if line.startswith("TRAIN"):
+            return "ok"
+        if not line:
+            return "timeout"
 
 def save_model_to_file(ser, filename="trained_weights.h"):
-    print(f"Requesting weight dump from Arduino")
+    print("Requesting final weight dump from Arduino.")
     ser.reset_input_buffer()
-
     ser.write(b'EX')
     ser.flush()
 
@@ -64,125 +76,114 @@ def save_model_to_file(ser, filename="trained_weights.h"):
             line = readline(ser)
             if "START_EXPORT" in line:
                 started = True
-                print("Receiving weights")
                 continue
             if "END_EXPORT" in line:
-                print(f"Model saved to {filename}")
                 break
             if started:
                 f.write(line + "\n")
-
+    print(f"Final model saved to {filename}")
     ser.write(b'R')
     ser.flush()
 
-
-def get_label(path):
-    folder = os.path.basename(os.path.dirname(path)).upper()
-    for class_name, idx in CLASSES.items():
-        if class_name in folder:
-            one_hot = [0] * N_CLASSES
-            one_hot[idx] = 1
-            return one_hot
-    return [0] * N_CLASSES
-
-
-def readline(ser):
-    try:
-        return ser.readline().decode("utf-8", errors="replace").strip()
-    except Exception:
-        return ""
-
-
-def wait_for_answer(ser):
-    while True:
-        line = readline(ser)
-        if line and "Probs" in line:
-            print(f"Arduino: {line}")
-        if line.startswith("TRAIN"):
-            return "ok"
-        if not line:
-            return "timeout"
-
-
-def stream_epoch(ser, epoch_idx, windows, labels):
+def stream_epoch(ser, epoch_idx, windows, labels, first_send_consumed=False):
     print(f"Starting Epoch {epoch_idx + 1}/{EPOCHS}")
-
     combined = list(zip(windows, labels))
     random.shuffle(combined)
 
     for i, (window, label) in enumerate(combined):
-        trigger = ""
-        while trigger != "SEND":
-            trigger = readline(ser)
+        if i == 0 and first_send_consumed:
+            pass
+        else:
+            trigger = ""
+            while trigger != "SEND":
+                trigger = readline(ser).strip()
 
-        window = (window - window.mean()) / (window.std() + 1e-8)
-        data_bytes = window.tobytes()
-
+        data_bytes = window.astype(np.float32).tobytes()
         for j in range(0, len(data_bytes), CHUNK_SIZE):
             chunk = data_bytes[j:j + CHUNK_SIZE]
             ser.write(chunk)
             ser.flush()
-            ack = readline(ser)
-            if ack != "ACK":
-                print(f"Err Ack: {ack}")
-                return
+            readline(ser)
 
-        ser.write(bytes(label))
+        one_hot = np.zeros(N_CLASSES, dtype=np.uint8)
+        one_hot[label] = 1
+        ser.write(one_hot.tobytes())
         ser.flush()
-        wait_for_answer(ser)
 
-        if i % 10 == 0:
-            print(f"Window {i}/{len(windows)} sent")
+        wait_for_answer(ser, i)
 
+        if i % 100 == 0:
+            print(f"Progress: Window {i}/{len(windows)}")
 
-if __name__ == '__main__':
-    search_pattern = "./data/RAT*_*"
-    all_folders = glob.glob(search_pattern)
+def prepare_dataset(rms_data, angles_ds):
+    rms_data = rms_data[:N_CHANNELS, :]
+    X = rms_data.T
 
-    files = [
-        "./data/RAT4_DORSIFLEXION/dorsi_170306_123322.rhd",
-        "./data/RAT4_PLANTARFLEXION/plantar_170306_123936.rhd",
-        "./data/RAT4_PRICKING/pricking_170306_125402.rhd",
-    ]
+    y = np.zeros(len(angles_ds), dtype=np.int64)
+    y[angles_ds > 2.0] = 1
+    y[angles_ds < -2.0] = 2
 
-    """files = []
-    for folder in all_folders:
-        folder_name = os.path.basename(folder).upper()
-        if any(cls in folder_name for cls in CLASSES.keys()):
-            rhd_files = glob.glob(os.path.join(folder, "*.rhd"))
-            files.extend(rhd_files)"""
+    X_seq, y_seq = [], []
 
-    all_windows = []
-    all_labels = []
+    for i in range(SEQ_LEN, len(X), SUBSAMPLE_RATE):
+        X_seq.append(X[i - SEQ_LEN:i].T)
+        y_seq.append(y[i])
 
-    for filename in files:
-        _, data = load_and_downsample(filename)
-        label = get_label(filename)
-        num_windows = data.shape[1]
-        for frame in range(num_windows):
-            window = data[:, frame, :].flatten().astype(np.float32)
-            window = (window - window.mean())
-            window_clipped = np.clip(window, -50.0, 50.0)
-            window_norm = window_clipped / 50.0
-            all_windows.append(window_norm)
-            all_labels.append(label)
+    return np.array(X_seq), np.array(y_seq)
+
+def load_rat(path):
+    data = np.load(path)
+    return data["rms_data"], data["a"]
+
+def build_dataset(data_dir, rat_ids):
+    X_all, y_all = [], []
+    for r in rat_ids:
+        path = os.path.join(data_dir, f"rat{r}.npz")
+        if os.path.exists(path):
+            rms, ang = load_rat(path)
+            X, y = prepare_dataset(rms, ang)
+            X_all.append(X)
+            y_all.append(y)
+    return np.concatenate(X_all), np.concatenate(y_all)
+
+if __name__ == "__main__":
+    data_dir = "./dataset_rats_50w"
+    rat_ids = list(range(4, 10))
+
+    print("Building subsampled dataset...")
+    X, y = build_dataset(data_dir, rat_ids)
+    print(f"New Dataset Size: {len(X)} windows")
+
+    n_channels = X.shape[1]
+    X_2d = X.transpose(0, 2, 1).reshape(-1, n_channels)
+    scaler = StandardScaler()
+    X_2d = scaler.fit_transform(X_2d)
+    X = X_2d.reshape(X.shape[0], X.shape[2], n_channels).transpose(0, 2, 1)
+
+    np.savez("scaler.npz", mean=scaler.mean_, scale=scaler.scale_)
 
     ser = serial.Serial(PORT, BAUD, timeout=5)
 
-    arduino_answer = ""
+    arduino_ready = False
     for _ in range(10):
-        line = readline(ser)
-        if line == "READY":
-            arduino_answer = "READY"
+        if readline(ser) == "READY":
+            ser.write(b"GO\n")
+            ser.flush()
+            arduino_ready = True
             break
 
-    if arduino_answer == "READY":
-        ser.write(b"GO\n")
-        ser.flush()
+    if not arduino_ready:
+        print("Arduino Connection Failed.")
+        exit()
+
+    while readline(ser).strip() != "SEND":
+        pass
+
+    try:
         for epoch in range(EPOCHS):
-            stream_epoch(ser, epoch, all_windows, all_labels)
-            save_model_to_file(ser, "trained_weights.h")
-        print("All epochs completed and model saved.")
-    else:
-        print("Arduino not ready.")
-        ser.close()
+            stream_epoch(ser, epoch, X, y, first_send_consumed=(epoch == 0))
+    except KeyboardInterrupt:
+        print("Training interrupted by user.")
+
+    save_model_to_file(ser, "trained_weights.h")
+    ser.close()
